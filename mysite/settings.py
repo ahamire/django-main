0ec11a0fc091f4c49d74ab81ea4b7e73ee8121e3
ahamire <gfueo3243@gmail.com>
2024-05-06 11:33:18 +0300

    sasd

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ['https://django-weather.up.railway.app/']
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
 # CSRF_TRUSTED_ORIGINS = ["*"]
@@ -80,16 +80,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'sypgfJFpaIBPAmwbwJOANJHYxlSLioQc',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '10258',
     }
 }
-
-
+POSTGRES_LOCALLY = False
+if POSTGRES_LOCALLY==True:
+    DATABASES['default'] = dj_database_url.parse('postgresql://postgres:sypgfJFpaIBPAmwbwJOANJHYxlSLioQc@roundhouse.proxy.rlwy.net:10258/railway')
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
